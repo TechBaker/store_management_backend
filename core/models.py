248acb7bad8fc193db87bc951e4a7815e05cf631
TechBaker <88749256+TechBaker@users.noreply.github.com>
2023-08-02 12:55:53 +0100

    Update models

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
     product_name = models.CharField(max_length=100)
     description = models.TextField()
     barcode = models.CharField(max_length=25, unique=True)
@@ -16,36 +16,47 @@ class Products(models.Model):
     def __str__(self):
         return f"{self.product_name}"
 
-class Customer(models.Model):
+class Client(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=40)
     phone_number = models.CharField(max_length=12)
     address = models.TextField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+class SaleItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
+    def __str__(self) -> str:
+        return f" Product: {self.product.product_name}, quantity: {self.quantity}, total: {self.total}"
+ 
 class Sale(models.Model):
+    client = models.OneToOneField(Client, on_delete=models.CASCADE)
     sale_date = models.DateTimeField(auto_now_add=True)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    products = models.ForeignKey(Products, on_delete=models.CASCADE)
+    items = models.ForeignKey(SaleItem, on_delete=models.CASCADE)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"Sale {self.id} - {self.sale_date}"
-
+   
 class Receipt(models.Model):
     sale = models.OneToOneField(Sale, on_delete=models.CASCADE)
     receipt_date = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    tax_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    # tax_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     discount_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"Receipt for Sale {self.sale_id} - {self.receipt_date}"
 
 class Invoice(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    sale = models.OneToOneField(Sale, on_delete=models.CASCADE)
     invoice_date = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField()
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
