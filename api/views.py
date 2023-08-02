45dba79c4dd251c1a762c8f34781a96dd0d185c5
TechBaker <88749256+TechBaker@users.noreply.github.com>
2023-08-02 18:14:23 +0100

    Add initial REST API

---
+++ api/views.py
@@ -0,0 +1,25 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from core.models import Product, Client
+from .serializers import ClientSerializer
+
+@api_view(['GET'])
+def getClients(request):
+    clients = Client.objects.all()
+    serializer = ClientSerializer(clients, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addClient(request):
+    serializer = ClientSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProducts(request):
+    pass
+
+@api_view(['POST'])
+def addProduct(request):
+    pass
